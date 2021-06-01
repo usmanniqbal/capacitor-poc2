39a4491f6966f791516e3b187606674eae91fdc7
Usman Iqbal <usman@eventmobi.com>
2021-06-01 12:54:15 -0400

    general changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useHistory } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/home';
 import AppUrlListener from './AppUrlListener';
+import { Plugins } from "@capacitor/core";
+const { App: CapApp } = Plugins;
 
 function App() {
   return (
-    <AppUrlListener></AppUrlListener>
-    // <BrowserRouter>
-    //   <div className="container mt-2" style={{ marginTop: 40 }}>
-    //     <AppUrlListener></AppUrlListener>
-    //     <Switch>
-    //       <Route exact path="/">
-    //         <Home />
-    //       </Route>
-    //       <Route path="/about">
-    //         <About />
-    //       </Route>
-    //     </Switch>
-    //   </div>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <div className="container mt-2" style={{ marginTop: 40 }}>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,24 +1,41 @@
 // pages/Home.js
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import AppUrlListener from "../AppUrlListener";
 import logo from '../logo.svg';
+import { Plugins } from "@capacitor/core";
+const { Browser, App } = Plugins;
 
-const Home = () => (
-    <div className="App">
-        <h1 className="title is-1">Native Pages</h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
-        </p>
+const Home = () => {
+    AppUrlListener();
 
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+    const onBrowser = async () => {
+        await Browser.open({
+            url: "https://capacitorjs.com/"
+        })
+    }
+    const onWindow = () => {
+        window.location.replace("/about");
+    }
+    return (
+        <div className="App">
+            <h1 className="title is-1">Native Pages</h1>
             <p>
-                Edit <code>src/App.js</code> and save to reload.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
             </p>
-            <Link to="/about">About</Link>
-        </header>
-    </div >
-);
+
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <button onClick={onBrowser}>Browser.open!</button>
+                <button onClick={onWindow}>window.open2!</button>
+                <Link to="/about">About</Link>
+            </header>
+        </div >
+    )
+};
 
 export default Home;
